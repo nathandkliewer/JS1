17ed15edc7e8000e18643cd8e4a39e2d20ef076d
nathankliewer <nathandkliewer@gmail.com>
2018-04-09 21:00:28 -0700

    worked on vars and alerts

---
+++ js/scripts.js
@@ -0,0 +1,9 @@
+var myHistory = " I want to learn this so I can rock and roll like Ryeker";
+console.log(myHistory);
+
+window.alert(myHistory);
+confirm(myHistory);
+console.log(myHistory);
+
+var myValue = confirm(myHistory);
+console.log(myValue);
