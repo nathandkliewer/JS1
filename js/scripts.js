1545e93e4adb10ea9748adb1a9c9249612f94848
nathankliewer <nathandkliewer@gmail.com>
2018-04-10 19:09:17 -0700

    day1 homework

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,13 @@
 var myHistory = " I want to learn this so I can rock and roll like Ryeker";
 console.log(myHistory);
 
+
+var myValue = confirm(myHistory);
+console.log(myValue);
+
+var myHistory = " What are my hobbies?";
+
+
 window.alert(myHistory);
 confirm(myHistory);
 console.log(myHistory);
