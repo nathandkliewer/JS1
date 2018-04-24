85265ea091e40d94805e7c8bd9061ce63a97e25e
nathankliewer <nathandkliewer@gmail.com>
2018-04-23 19:47:48 -0700

    update

---
+++ js/scripts.js
@@ -0,0 +1,52 @@
+/*
+their name favorite color basic 10 colors
+/
+
+var favColor = prompt (how about dat fav colly, doe. )
+*/
+let shoppingList = [];
+let item1 = "detergent";
+let item2 = "cheese";
+let item3 = "crackas";
+let item4 = "wine";
+
+shoppingList.push(item1, item2, item3, item4);
+/*console.log(shoppingList) */
+
+var favoriteMovie = ['The Shawshank Redemption, Andy Dufresne: '];
+   var secondFavorite = ['Point of No Return 1993 Bridget Fonda '];
+   var thirdFavorite = ['Star Wars', 'Luke'];
+   var thirdFavorite = ['Wizard of OZ', 'Dorthy'];
+   var fourthFavorite = ['Dallas', 'Whoever shot jr'];
+   var fifthFavorite = ['Duke of Hazzard', 'Uncle Jesse'];
+
+   var myFavoriteMovies = [];
+   myFavoriteMovies.push(favoriteMovie, secondFavorite, thirdFavorite,fourthFavorite,fifthFavorite);
+   //Result:
+
+let i = 0;
+while ( i< myFavoriteMovie.length) {
+console.log(myFavoriteovies[i][0]);
+i++
+
+}
+   console.log(myFavoriteMovie )
+
+var movieName = [];
+movieYears.push(myFavoriteMovies[0][1], myFavoriteMovies[1][1],myFavoriteMovies[2][1]);
+alert(movieYears);
+
+const titles = [];
+const favAndLeastFav = [];
+
+title.push(favoriteMovie[0],secondFavorite [0], thirdFavorite [0], fifthFavorite [0] )
+
+
+var str = "";
+
+for (var i = 0; i < 9; i++) {
+  str = str + i;
+}
+
+console.log(str);
+// expected output: "012345678"
