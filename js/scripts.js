977cfcb0f86c1af6d0b0b800d6a8f34bca1b7f9d
nathankliewer <nathandkliewer@gmail.com>
2018-04-25 18:44:31 -0700

    update2

--- js/scripts.js
+++ js/scripts.js
@@ -1,10 +1,19 @@
-(function(){
-  "use strict";
-  /* Start of your code */
-  function greetMe(yourName) {
-    alert('Hello ' + yourName);
-  }
+// feilds
+let emailArr;
+var yourEmail;
 
-  greetMe('World');
-  /* End of your code */
-})();
+while (true) {
+  //user input
+  var yourEmail = prompt('What is your email?');
+  emailArr = yourEmail.split("");
+
+  if (!(emailArr.includes('@'))) {
+    var yourEmail = prompt('Did you forgot the "@"? Please try again.');
+  }
+  else if (!(emailArr.includes('.'))) {
+    var yourEmail = prompt('That was not a valid email. Please try again.');
+  }
+  else {
+    alert('Thanks for providing a valid email.');
+  }
+}
