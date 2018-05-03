19c7143b40c69a63ddb6a4fca7a7203261b18757
nathankliewer <nathandkliewer@gmail.com>
2018-05-02 19:53:15 -0700

    update 1

--- js/scripts.js
+++ js/scripts.js
@@ -1,2 +1,14 @@
-var myHeading = document.querySelector('h1');
-myHeading.textContent = 'Hello world!';
+var myVariable = document.querySelector('h1');
+ var myName = prompt('What is your name? .');
+ var iceCream = prompt('What is favorite Ice Cream? .');
+console.log(iceCream);
+var iceCream = '';
+if (iceCream === 'chocolate')
+
+{
+  alert('Yay, I love chocolate ice cream!');
+}
+else {
+  alert('Awwww, but chocolate is my favorite...');
+}
+alert(`Hey ${myName}, ${iceCream}`)
