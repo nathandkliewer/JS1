28fbee479088dd883feb2df70dc15a4acbc6ede3
nathankliewer <nathandkliewer@gmail.com>
2018-05-06 09:41:02 -0700

    tying this

--- 3dayhomework/js/scripts.js
+++ 3dayhomework/js/scripts.js
@@ -1,1 +1,9 @@
-//DAY 6 GOES HERE
+var myVariable = document.querySelector('h1');
+ var myName = prompt('What is your name? .');
+ var thingTodo1 = prompt('What do you need to do today? .');
+
+var thingTodo2 = prompt('What 2nd thing you need to do today? .');
+
+
+
+alert(`Hey ${myName}, Here are the things you need to do today ${thingTodo1}`)
